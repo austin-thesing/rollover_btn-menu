2f34807dc2b518f694c405b9d6669ab63d2ad020
Austin <admin@designxdevelop.com>
2016-03-14 14:52:59 -0700

    Added on mouseover menu pops up.. mouseoff action is not working yet.. I need to add some time delay for the menu rollover effect.

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.play-btn').mouseover(function(){
+    $('.next-menu').show();
+  });
+  $('.play-btn').mouseoff(function(){
+    $('.next-menu').hide();
+  });
+});
